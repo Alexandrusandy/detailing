accd3fcc023d817ff1949695a7c0950ab19d2889
Alexandru <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-26 14:33:40 +0200

    Add files via upload

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavDropdown,Navbar,Nav, } from 'react-bootstrap';
-
+import {NavLink} from 'react-router-dom';
 import { Link } from "react-scroll";
 
 
 const Navigation = () => {
-	return(
+  return(
    
     <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark" >
   <Navbar.Brand href="./poza/Poza.js">Acasa</Navbar.Brand>
@@ -18,51 +18,28 @@ const Navigation = () => {
     to="despre-noi"
     spy={true}
     smooth={true}
-    offset={-240}
+    offset={-70}
     duration={500}
 >
-      <Nav.Link href="/Despre">Despre Noi</Nav.Link> </Link>
+      <Nav.Link href="/Pretu">Despre Noi</Nav.Link> </Link>
        <Link
     activeClass="active"
     to="ServiciiID"
     spy={true}
     smooth={true}
-    offset={-240}
+    offset={-70}
     duration={500}>
       <Nav.Link activeClassName="active" href="/Servicii">Servicii</Nav.Link></Link>
-      <Link
-    activeClass="active"
-    to="galerie"
-    spy={true}
-    smooth={true}
-    offset={-240}
-    duration={500}
->      <Nav.Link href="./Galerie/Galerie">Galerie Foto</Nav.Link>
-    </Link>
-    </Nav>
-    <Nav>
-    <NavDropdown title="Preturi" id="collasible-nav-dropdown">
-       <Link
-    activeClass="active"
-    to="interior"
-    spy={true}
-    smooth={true}
-    offset={-240}
-    duration={500}
->  
-<NavDropdown.Item href="./Preturiinterior">Detaiing Interior</NavDropdown.Item>   
-    </Link> 
+      <NavDropdown title="Preturi" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/Preturiinterior">Detaiing Interior</NavDropdown.Item>     
         <NavDropdown.Item href="./Preturi/Preturi.html">Detailing Exterior</NavDropdown.Item>
       </NavDropdown>
-     <Link
-    activeClass="active"
-    to="contact"
-    spy={true}
-    smooth={true}
-    offset={-240}
-    duration={500}
->      <Nav.Link  href="./Contact/Contact"> Contact</Nav.Link>
-</Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="/Galerie">Galerie Foto</Nav.Link>
+      <Nav.Link  href="/Contact">
+        Contact
+      </Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
