912deebc08d1b373a7758e762a5a8b57641b353b
Alexandru <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-27 20:28:01 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import Poza from './poza/Poza';
-import Despre from './despre/despre';
-import Servicii from './Servicii/Servicii';
+import {BrowserRouter,Link,Switch,Route} from 'react-router-dom';
 import Preturiinterior from './Preturi/Preturiinterior';
 import Galerie from './Galerie/Galerie';
 import Contact from './Contact/Contact';
 
-ReactDOM.render(
-  <Router>
-    <App >
-    <Switch>
-     <Route path='/Preturiinterior' component={Preturiinterior}/>
-     <Route path='/Servicii' component={Servicii}/>
-     <Route path='/Galerie' component={Galerie}/>
-     <Route path='/Contact' component={Contact}/>
-    <Poza /> 
-    </Switch>
-    </App>
-  </Router>,
-  document.getElementById('root')
-);
+function App(){
+  return(
+    <div>
+      <h2>Welcome to App Component...</h2>
+      <ul>
+        <li><Link to="/">Employees</Link></li>
+        <li><Link to="/Preturiinterior">Preturiinterior</Link></li>
+        <li><Link to="/Galerie">Galerie</Link></li>
+        <li><Link to="/Contact">Contact</Link></li>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Contact}></Route>
+        <Route path="/Preturiinterior" component={Preturiinterior}></Route>
+        <Route path="/Galerie" component={Galerie}></Route>
+        <Route path="/Contact" component={Contact}></Route>
+      </Switch>
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<BrowserRouter><App></App></BrowserRouter>,document.getElementById("root"));
